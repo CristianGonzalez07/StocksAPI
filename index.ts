de230c6c2cfa291fc1f273758bbef0ced0784f3c
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-06-14 21:28:58 -0300

    cors management added

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 dotenv.config();
 
 const port = process.env.PORT;
@@ -8,6 +10,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
